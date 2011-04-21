87bb1f5f8c53e4aa94df6560fe284b23135f03e5
potench <christian.harden@gmail.com>
2011-04-21 16:31:22 -0700

    clearing conflicst from fork merge

--- media/js/site/common/site.js
+++ media/js/site/common/site.js
@@ -46,22 +46,12 @@ RED.SITE = $.extend(RED.SITE, function () {
 			// Setup History Events
 			RED.require("history", this);
 			
-<<<<<<< HEAD
-			$(document).ready(this.onReady.call(this));
-=======
 			$(document).ready(this.onReady);
->>>>>>> a1e654b44d9dc6e2592f1df71c413a49def38ca5
 		},
 		
 		onReady : function () {
 			var body = $(document.body);
 			
-<<<<<<< HEAD
-			this.load([{
-				test : $,
-				yep : {
-					"Global" : "/media/js/site/common/global.js"
-=======
 			Modernizr.load([{
 				test : $,
 				yep : {
@@ -71,61 +61,30 @@ RED.SITE = $.extend(RED.SITE, function () {
 					if (result) {
 						RED.HBO.models.Global = new RED.HBO.Global();
 					}
->>>>>>> a1e654b44d9dc6e2592f1df71c413a49def38ca5
 				}
 			}, {
 				test : body.hasClass("home"),
 				yep : {
-<<<<<<< HEAD
-					"Home" : "/media/js/site/home.js"
-=======
 					"Home" : SYS.MEDIA_URL + "js/site/home.js"
 				},
 				callback : function (url, result, key) {
 					if (result) {
 						RED.HBO.models.Home = new RED.HBO.Home();
 					}
->>>>>>> a1e654b44d9dc6e2592f1df71c413a49def38ca5
 				}
 			}, {
 				test : body.hasClass("about"),
 				yep : {
-<<<<<<< HEAD
-					"About" : "/media/js/site/about.js"
-=======
 					"About" : SYS.MEDIA_URL + "js/site/about.js"
 				},
 				callback : function (url, result, key) {
 					if (result) {
 						RED.HBO.models.About = new RED.HBO.About();
 					}
->>>>>>> a1e654b44d9dc6e2592f1df71c413a49def38ca5
 				}
 			}, {
 				test : body.hasClass("contact"),
 				yep : {
-<<<<<<< HEAD
-					"Contact" : "/media/js/site/contact.js"
-				}
-			}]);
-		},
-		
-		load : function (stack) {
-			for (var i = 0, j = stack.length; i < j; i++) {
-				stack[i].callback = this.onScriptLoad;
-			}
-			
-			return Modernizr.load(stack);
-		},
-		
-		onScriptLoad : function (url, result, key) {
-			if (result) {
-				console.group("new RED.SITE.Page." + key);
-					RED.SITE.models[key] = new RED.SITE[key]();
-					console.log("RED.SITE." + key, RED.SITE.models[key]);
-				console.groupEnd();
-			}
-=======
 					"Contact" : SYS.MEDIA_URL + "js/site/contact.js"
 				},
 				callback : function (url, result, key) {
@@ -135,7 +94,6 @@ RED.SITE = $.extend(RED.SITE, function () {
 				}
 			}]);
 			
->>>>>>> a1e654b44d9dc6e2592f1df71c413a49def38ca5
 		},
 		
 		refresh : function (state) {
