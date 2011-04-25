11d98c94e73bf9c0b65813493018d4f498624334
Richard Herrera <rich@doctyper.com>
2011-04-25 12:50:38 -0700

    Assume jQuery 1.5.

--- example/site.js
+++ example/site.js
@@ -40,7 +40,7 @@ RED.SITE = $.extend(true, Example, RED, function () { // inherit the RED framewo
 		
 		onReady : function () {
 			var body = $("body"),
-				pageClass = (body.data("page-class") || body.attr("data-page-class")); // use attr("data-page-class") if < jquery 1.5
+				pageClass = body.data("page-class"); // use attr("data-page-class") if < jquery 1.5
 			
 			this.page = new (Example.Page[pageClass] || Example.Page)(); // creates Page() based on <div data-page-class="Home">, defaults to Example.Page();
 			console.group("page", pageClass, this.page);
