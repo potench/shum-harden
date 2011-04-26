b8db2e106b494ced2fcffa4b194dd53ce08aa7f2
Richard Herrera <rich@doctyper.com>
2011-04-25 19:00:11 -0700

    Updating paths to static files.

--- config.rb
+++ config.rb
@@ -1,10 +1,10 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../../static/media/css"
+css_dir = "../../static/css"
 sass_dir = "sass"
-images_dir = "../../static/media/img"
-javascripts_dir = "../../media/static/js"
+images_dir = "../../static/img"
+javascripts_dir = "../../static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
