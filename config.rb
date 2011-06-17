c2b08038dd9d71394cb6df47f4d135806f16c7a6
Richard Herrera <rich@doctyper.com>
2011-06-16 17:46:36 -0700

    Enabling relative_assets by default.

--- config.rb
+++ config.rb
@@ -10,7 +10,7 @@ javascripts_dir = "../../project/static/js"
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :expanded
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
 
