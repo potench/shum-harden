d64c0411ee5a86e08258a7ff782676d89860d674
Richard Herrera <rich@doctyper.com>
2011-06-16 18:24:02 -0700

    Adding the Shell namespace. Used for static behavior that does not need to be instantiated per-page.

---
+++ example/shell.js
@@ -0,0 +1,45 @@
+// ### Part of the [Rosy Framework](http://github.com/ff0000/rosy)
+/* shell.js */
+
+// Custom [JSLint](http://jslint.com) settings.
+/*global $: true, console: true, Class: true */
+/*jslint browser: true, onevar: true */
+
+// ## The RED Namespace
+var RED = RED || {};
+
+// ## Local Namespace
+// Example Site object, controls global functionality and instantiates the Example Default Page.
+// You should replace the namespace "Example" with your own Site namespace, this is only an example.
+var Example = Example || {};
+
+// Site shell object
+Example.Shell = (function () {
+	
+	// <this scope="RED">
+	// </this>
+	
+	// <this scope="Example.Shell">
+	
+	return Example.Page.extend({
+		vars : {},
+		
+		/*
+		Function: Example.Shell.scope
+		*/
+		init : function () {
+			this.setupHeader();
+			this.setupFooter();
+			
+			this.sup();
+		},
+
+		setupHeader : function () {},
+		
+		setupFooter : function () {}
+
+	});
+	
+	// </this>
+	
+}.call(RED));
