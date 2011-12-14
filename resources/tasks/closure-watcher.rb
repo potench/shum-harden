b1c83c28303b1547613d2e008e4616fa36a62c95
Richard Herrera <rich@doctyper.com>
2011-12-14 08:54:59 -0800

    Closure watcher updates:
    - On first boot, compile a group only if source files are newer than output file (or if no output file is found).
    - Correct gzip output. Previously it would always report "2b".
    - If a group in setup.json is called "exclude", ignore it.

--- resources/tasks/closure-watcher.rb
+++ resources/tasks/closure-watcher.rb
@@ -22,61 +22,97 @@ class Rosy::Watch::Closure
     @json = JSON.parse(File.read(@file)) if match.eql?(@file)
 
     @json.each_pair do |key, group|
+      self.build key, group, relative, match
+    end
+  end
+
+  def build(key, group, relative = nil, match = nil)
+    source = group["source"].to_a
+    output = group["output"].to_s
+
+    return if key.eql?("exclude") or output.nil?
+
+    if match.eql?(@file) or source.include?(match) or relative.nil?
+      puts ">>> #{(relative.nil? || match.eql?(@file)) ? "Compiling" : "Change detected to: #{relative} in"} group \"#{key}\""
+
+      code = String.new
+
+      begin
+        source.each do |file|
+          current = File.join(STATIC_DIR, file)
+
+          if File.exists?(current)
+            if file.eql?(match) or @compiled[file].nil?
+              puts "    #{cyan("compile")} #{file}"
+              @compiled[file] = Closure::Compiler.new.compile(File.read(current))
+            end
+
+            code << @compiled[file]
+          else
+            puts "    #{red("missing")} #{file}"
+          end
+        end
+      rescue Closure::Error => error
+        puts "    #{red("error:")}"
+        puts error
+      else
+        output_file = File.join(STATIC_DIR, output)
+        output_file_exists = File.exists?(output_file)
+        existing = File.read(output_file) if output_file_exists
+
+        if code.eql?(existing)
+          puts "#{green("identical")} #{output}"
+        else
+          FileUtils.mkpath(File.dirname(output_file)) unless File.exists?(output_file)
+
+          File.open(output_file, "w") do |f|
+            f.write(code)
+
+            gzip = Zlib::Deflate.new
+            deflate = gzip.deflate(code, Zlib::FINISH)
+            gzip.close
+
+            output_str = "#{output} #{yellow(">>>")} #{cyan("#{self.filesize(code.size)} (#{self.filesize(deflate.size)} gzip)")}"
+
+            if (output_file_exists)
+              puts "#{yellow("overwrite")} #{output_str}"
+            else
+              puts "#{green("create")} #{output_str}"
+            end
+          end unless code.nil?
+        end
+      end
+
+    end
+  end
+
+  def update
+    # More expensive, but allows for live editing of JSON file
+    @json = JSON.parse(File.read(@file))
+
+    @json.each_pair do |key, group|
       source = group["source"].to_a
       output = group["output"].to_s
 
-      if match.eql?(@file) or source.include?(match) or relative.nil?
-        puts ">>> #{(relative.nil? || match.eql?(@file)) ? "Compiling" : "Change detected to: #{relative} in"} group \"#{key}\""
+      output_file = File.join(STATIC_DIR, output)
 
-        code = String.new
+      if File.exists?(output_file)
+        source.each do |file|
+          current = File.join(STATIC_DIR, file)
 
-        begin
-          source.each do |file|
-            current = File.join(STATIC_DIR, file)
+          output_modified_time = File.ctime(output_file)
 
-            if File.exists?(current)
-              if file.eql?(match) or @compiled[file].nil?
-                puts "    #{cyan("compile")} #{file}"
-                @compiled[file] = Closure::Compiler.new.compile(File.read(current))
-              end
-
-              code << @compiled[file]
-            else
-              puts "    #{red("missing")} #{file}"
+          if File.exists?(current)
+            if File.ctime(current) > output_modified_time
+              self.build key, group
+              break
             end
-          end
-        rescue Closure::Error => error
-          puts "    #{red("error:")}"
-          puts error
-          break
-        else
-          output_file = File.join(STATIC_DIR, output)
-          output_file_exists = File.exists?(output_file)
-          existing = File.read(output_file) if output_file_exists
-
-          if code.eql?(existing)
-            puts "#{green("identical")} #{output}"
           else
-            FileUtils.mkpath(File.dirname(output_file)) unless File.exists?(output_file)
-
-            File.open(output_file, "w") do |f|
-              f.write(code)
-
-              gzip = Zlib::Deflate.new
-              deflate = gzip.deflate(code)
-              gzip.close
-
-              output_str = "#{output} #{yellow(">>>")} #{cyan("#{self.filesize(code.size)} (#{self.filesize(deflate.size)} gzip)")}"
-
-              if (output_file_exists)
-                puts "#{yellow("overwrite")} #{output_str}"
-              else
-                puts "#{green("create")} #{output_str}"
-              end
-            end unless code.nil?
+            puts "    #{red("missing")} #{file}"
           end
         end
-
+      else
+        self.build key, group
       end
     end
   end
@@ -109,9 +145,10 @@ class Rosy::Watch::Closure
   def run
     puts ">>> Polling for JavaScript changes. Press Ctrl-C to Stop"
 
-    # Run once on initialization
     block = self
-    block.compile
+
+    # Run once on initialization
+    block.update
 
     FSSM.monitor do
       path PROJECT_ROOT do
